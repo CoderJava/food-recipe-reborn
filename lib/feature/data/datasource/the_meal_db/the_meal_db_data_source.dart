da8ec260a4619f6707ec0953890c1c0c2f38f039
CoderJava <kolonel.yudisetiawan@gmail.com>
2025-07-04 21:35:38 +0700

    Buat endpoint getCategory
    Sekalian buat unit testnya.

--- lib/feature/data/datasource/the_meal_db/the_meal_db_data_source.dart
+++ lib/feature/data/datasource/the_meal_db/the_meal_db_data_source.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:food_recipe/config/base_url_config.dart';
+import 'package:food_recipe/feature/data/model/category/category_response.dart';
 import 'package:food_recipe/feature/data/model/meal/meal_response.dart';
 
 abstract class TheMealDbDataSource {
@@ -9,6 +10,13 @@ abstract class TheMealDbDataSource {
   late String pathGetRandomMeal;
 
   Future<MealResponse> getRandomMeal();
+
+  /// panggil endpoint [host]/categories.php
+  ///
+  /// throws [DioException] untuk semua error kode
+  late String pathGetCategory;
+
+  Future<CategoryResponse> getCategory();
 }
 
 class TheMealDbDataSourceImpl implements TheMealDbDataSource {
@@ -31,6 +39,22 @@ class TheMealDbDataSourceImpl implements TheMealDbDataSource {
       return MealResponse.fromJson(response.data);
     } else {
       throw DioException(requestOptions: RequestOptions(path: pathGetRandomMeal));
+    }
+  }
+
+  @override
+  String pathGetCategory = '';
+
+  @override
+  Future<CategoryResponse> getCategory() async {
+    pathGetCategory = '$baseUrl/categories.php';
+    final response = await dio.get(
+      pathGetCategory,
+    );
+    if (response.statusCode.toString().startsWith('2')) {
+      return CategoryResponse.fromJson(response.data);
+    } else {
+      throw DioException(requestOptions: RequestOptions(path: pathGetCategory));
     }
   }
 }
--- test/feature/data/datasource/the_meal_db/the_meal_db_data_source_test.dart
+++ test/feature/data/datasource/the_meal_db/the_meal_db_data_source_test.dart
@@ -4,6 +4,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:food_recipe/config/base_url_config.dart';
 import 'package:food_recipe/feature/data/datasource/the_meal_db/the_meal_db_data_source.dart';
+import 'package:food_recipe/feature/data/model/category/category_response.dart';
 import 'package:food_recipe/feature/data/model/meal/meal_response.dart';
 import 'package:mockito/mockito.dart';
 
@@ -89,6 +90,76 @@ void main() {
 
         // act
         final call = remoteDataSource.getRandomMeal();
+
+        // assert
+        expect(() => call, throwsA(const TypeMatcher<DioException>()));
+      },
+    );
+  });
+
+  group('getCategory', () {
+    const tPathResponse = 'category_response.json';
+    final tResponse = CategoryResponse.fromJson(
+      json.decode(
+        fixture(tPathResponse),
+      ),
+    );
+
+    void setUpMockDioSuccess() {
+      final responsePayload = json.decode(fixture(tPathResponse));
+      final response = Response(
+        requestOptions: tRequestOptions,
+        data: responsePayload,
+        statusCode: 200,
+        headers: Headers.fromMap({
+          Headers.contentTypeHeader: [Headers.jsonContentType],
+        }),
+      );
+      when(mockDio.get(any)).thenAnswer((_) async => response);
+    }
+
+    test(
+      'pastikan endpoint benar-benar terpanggil dengan method GET',
+      () async {
+        // arrange
+        setUpMockDioSuccess();
+
+        // act
+        await remoteDataSource.getCategory();
+
+        // assert
+        verify(mockDio.get('$baseUrl/categories.php'));
+      },
+    );
+
+    test(
+      'pastikan mengembalikan objek class model ketika menerima respon sukses '
+      'dari endpoint',
+      () async {
+        // arrange
+        setUpMockDioSuccess();
+
+        // act
+        final result = await remoteDataSource.getCategory();
+
+        // assert
+        expect(result, tResponse);
+      },
+    );
+
+    test(
+      'pastikan akan menerima exception DioError ketika menerima respon kegagalan dari endpoint',
+      () async {
+        // arrange
+        final response = Response(
+          requestOptions: tRequestOptions,
+          data: 'Bad Request',
+          statusCode: 400,
+        );
+        when(mockDio.get(any)).thenAnswer((_) async => response);
+
+        // act
+        final call = remoteDataSource.getCategory();
 
         // assert
         expect(() => call, throwsA(const TypeMatcher<DioException>()));
