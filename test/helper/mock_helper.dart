4e6771b49ec55f8dac0ee0c87fcdbe630ef077c4
CoderJava <kolonel.yudisetiawan@gmail.com>
2025-07-04 21:54:19 +0700

    Daftarkan use case `GetCategory` kedalam mock_helper.dart

--- test/helper/mock_helper.dart
+++ test/helper/mock_helper.dart
@@ -5,6 +5,7 @@ import 'package:food_recipe/core/util/helper.dart';
 import 'package:food_recipe/core/util/shared_preferences_manager.dart';
 import 'package:food_recipe/feature/data/datasource/the_meal_db/the_meal_db_data_source.dart';
 import 'package:food_recipe/feature/domain/repository/the_meal_db/the_meal_db_repository.dart';
+import 'package:food_recipe/feature/domain/usecase/get_category/get_category.dart';
 import 'package:food_recipe/feature/domain/usecase/get_random_meal/get_random_meal.dart';
 import 'package:mockito/annotations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -20,5 +21,6 @@ import 'package:shared_preferences/shared_preferences.dart';
   MockSpec<TheMealDbDataSource>(),
   MockSpec<TheMealDbRepository>(),
   MockSpec<GetRandomMeal>(),
+  MockSpec<GetCategory>(),
 ])
 void main() {}
