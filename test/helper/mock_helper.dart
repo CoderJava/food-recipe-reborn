c6b1407aaf9d385802846ae2c137f1bbe9ba73ed
CoderJava <kolonel.yudisetiawan@gmail.com>
2025-07-04 21:03:05 +0700

    Daftarkan use case GetRandomMeal kedalam mock_helper.dart

--- test/helper/mock_helper.dart
+++ test/helper/mock_helper.dart
@@ -5,6 +5,7 @@ import 'package:food_recipe/core/util/helper.dart';
 import 'package:food_recipe/core/util/shared_preferences_manager.dart';
 import 'package:food_recipe/feature/data/datasource/the_meal_db/the_meal_db_data_source.dart';
 import 'package:food_recipe/feature/domain/repository/the_meal_db/the_meal_db_repository.dart';
+import 'package:food_recipe/feature/domain/usecase/get_random_meal/get_random_meal.dart';
 import 'package:mockito/annotations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -18,5 +19,6 @@ import 'package:shared_preferences/shared_preferences.dart';
   MockSpec<Helper>(),
   MockSpec<TheMealDbDataSource>(),
   MockSpec<TheMealDbRepository>(),
+  MockSpec<GetRandomMeal>(),
 ])
 void main() {}
