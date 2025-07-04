4c5df6a77f6c5ee9891378419d35024abb5398bb
CoderJava <kolonel.yudisetiawan@gmail.com>
2025-07-04 20:40:23 +0700

    Buat file network_info.dart
    Sekalian buat unit testnya.

---
+++ test/core/network/network_info_test.dart
@@ -0,0 +1,67 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:food_recipe/core/network/network_info.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../helper/mock_helper.mocks.dart';
+
+void main() {
+  late NetworkInfoImpl networkInfoImpl;
+  late MockConnectivity mockConnectivity;
+
+  setUp(() {
+    mockConnectivity = MockConnectivity();
+    networkInfoImpl = NetworkInfoImpl(mockConnectivity);
+  });
+
+  test(
+    'pastikan fungsi checkConnectivity benar-benar terpanggil dengan kondisi koneksi '
+    'menggunakan mobile data',
+    () async {
+      // arrange
+      final tHasConnection = [ConnectivityResult.mobile];
+      when(mockConnectivity.checkConnectivity()).thenAnswer((_) async => tHasConnection);
+
+      // act
+      final result = await networkInfoImpl.isConnected;
+
+      // assert
+      verify(mockConnectivity.checkConnectivity());
+      expect(result, true);
+    },
+  );
+
+  test(
+    'pastikan fungsi checkConnectivity benar-benar terpanggil dengan kondisi koneksi '
+    'menggunakan wifi',
+    () async {
+      // arrange
+      final tHasConnection = [ConnectivityResult.wifi];
+      when(mockConnectivity.checkConnectivity()).thenAnswer((_) async => tHasConnection);
+
+      // act
+      final result = await networkInfoImpl.isConnected;
+
+      // assert
+      verify(mockConnectivity.checkConnectivity());
+      expect(result, true);
+    },
+  );
+
+  test(
+    'pastikan fungsi checkConnectivity benar-benar terpanggil dengan kondisi koneksi '
+    'tidak terhubung sama sekali',
+    () async {
+      // arrange
+      final tHasConnection = [ConnectivityResult.none];
+      when(mockConnectivity.checkConnectivity()).thenAnswer((_) async => tHasConnection);
+
+      // act
+      final result = await networkInfoImpl.isConnected;
+
+      // assert
+      verify(mockConnectivity.checkConnectivity());
+      expect(result, false);
+    },
+  );
+}
