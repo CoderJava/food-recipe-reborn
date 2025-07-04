8adc72a14d2b243e0c7323100668009b24556e3e
CoderJava <kolonel.yudisetiawan@gmail.com>
2025-07-04 20:47:45 +0700

    Buat file mock_helper.dart

---
+++ test/helper/mock_helper.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:food_recipe/core/network/network_info.dart';
+import 'package:food_recipe/core/util/helper.dart';
+import 'package:food_recipe/core/util/shared_preferences_manager.dart';
+import 'package:food_recipe/feature/data/datasource/the_meal_db/the_meal_db_data_source.dart';
+import 'package:mockito/annotations.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+@GenerateNiceMocks([
+  MockSpec<Connectivity>(),
+  MockSpec<NetworkInfo>(),
+  MockSpec<SharedPreferences>(),
+  MockSpec<SharedPreferencesManager>(),
+  MockSpec<Dio>(),
+  MockSpec<HttpClientAdapter>(),
+  MockSpec<Helper>(),
+  MockSpec<TheMealDbDataSource>(),
+])
+void main() {}
