d441b24d0c2a9ff8fb8b9ab94598e5a3b7262185
CoderJava <kolonel.yudisetiawan@gmail.com>
2025-07-04 21:00:58 +0700

    Daftarkan `TheMealDbRepository` kedalam mock_helper.dart

--- test/helper/mock_helper.dart
+++ test/helper/mock_helper.dart
@@ -4,6 +4,7 @@ import 'package:food_recipe/core/network/network_info.dart';
 import 'package:food_recipe/core/util/helper.dart';
 import 'package:food_recipe/core/util/shared_preferences_manager.dart';
 import 'package:food_recipe/feature/data/datasource/the_meal_db/the_meal_db_data_source.dart';
+import 'package:food_recipe/feature/domain/repository/the_meal_db/the_meal_db_repository.dart';
 import 'package:mockito/annotations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -16,5 +17,6 @@ import 'package:shared_preferences/shared_preferences.dart';
   MockSpec<HttpClientAdapter>(),
   MockSpec<Helper>(),
   MockSpec<TheMealDbDataSource>(),
+  MockSpec<TheMealDbRepository>(),
 ])
 void main() {}
